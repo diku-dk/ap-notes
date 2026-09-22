1bbf36f2b2668d5ce0a78af939d884d4d0fe7f1a
Troels Henriksen <athas@sigkill.dk>
2026-09-22 13:27:12 +0200

    Pin mdbook.

--- shell.nix
+++ shell.nix
@@ -1,4 +1,16 @@
-{ pkgs ? import <nixpkgs> {} }:
+{
+  pkgs ?
+    import
+      (fetchTarball {
+        # Pinned because mdbook 0.5 broke mdbook-admonish's preprocessor
+        # protocol (https://github.com/tommilligan/mdbook-admonish/issues/233),
+        # and no fixed mdbook-admonish release exists yet. This revision has
+        # mdbook 0.4.52, the last version compatible with mdbook-admonish 1.20.0.
+        url = "https://github.com/NixOS/nixpkgs/archive/f575f2e29d4d27bfbe1887dca8f37611f1297cdf.tar.gz";
+        sha256 = "1hh6r8zdp3arjdf4s7v82xvmn9zxdbkap0kb7yai65hd321bhss2";
+      })
+      { }
+}:
 pkgs.stdenv.mkDerivation {
   name = "shell";
   buildInputs =
